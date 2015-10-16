5569a067f96151d16dd36f784ee6ecefa3c071f9
christian <christian@lefty.io>
2015-10-16 11:34:37 +0200

    Adding the initial implementation for the HBASE grpc server. Supports gets and puts.

---
+++ protoc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/usr/local/bin/protoc --java_out=client/src/main/java --proto_path=. --proto_path=/usr/local/include                  \
+    --java_plugin_out=client/src/main/java                                                                            \
+    --plugin=protoc-gen-java_plugin=/opt/grpc-java/compiler/build/binaries/java_pluginExecutable/protoc-gen-grpc-java \
+    proto/hbase.proto
